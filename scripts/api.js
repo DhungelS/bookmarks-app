9813388c41bde435c202999fbddb2a968169ef0a
Saugat Dhungel <saugat.dhungel@gmail.com>
2018-01-26 09:21:08 -0500

    expand view toggle working

--- scripts/api.js
+++ scripts/api.js
@@ -1,1 +1,46 @@
+'use strict';
 //Includes POST, GET, PATCH, AND DELETE requests to the API
+
+const api = (function () {
+
+  const BASE_URL = 'https://thinkful-list-api.herokuapp.com/saugat'
+
+  const getItems = function (callback) {
+    $.getJSON(`${BASE_URL}/bookmarks`, callback);
+  };
+
+  const createItem = function (itemData, callback) {
+    
+    const item = JSON.stringify(itemData);
+
+    const callSettings = {
+      url: `${BASE_URL}/bookmarks`,
+      method: 'POST',
+      contentType: 'application/json',
+      data: item,
+      success: callback
+    };
+
+    $.ajax(callSettings);
+
+  };
+
+  const deleteItem = function(id, callback){
+    $.ajax({
+      url: `${BASE_URL}/bookmarks/${id}`,
+      method: 'DELETE',
+      success: callback
+    });
+  };
+
+  return {
+    getItems,
+    createItem,
+    deleteItem
+  };
+
+}());
+
+api.getItems(item => {
+  console.log(item);
+});
--- scripts/bookmarks.js
+++ scripts/bookmarks.js
@@ -1,3 +1,5 @@
+/* global api */
+
 'use strict';
 
 //Include all the DOM functions such as event listeners, adding new items to the DOM etc.
@@ -7,23 +9,106 @@ const store = {
 };
 
 function generateBookmarkElement(item) {
-  return (`<li></li>`);
+
+  let displayToggle = 'info'
+  
+  if (item.expandedView === true) {
+    displayToggle = 'expanded'
+  }
+
+  return (
+    `<li data-item-id="${item.id}" class="bookmark-item">
+    <p>${item.title}<p>
+    <p>${item.rating}</p>
+    <div class="${displayToggle}"> 
+    <p>${item.url}</p>
+    <p>${item.desc}</p>
+    </div>
+    <button id="delete-bookmark" type="button">delete</button>
+    <button id="expand-bookmark" type="button">View in Full</button>
+    </li>`
+
+  );
+
 }
 
 function generateBookmarkString() {
 
+  return store.bookmarks.map(bookmark => {
+    return generateBookmarkElement(bookmark);
+  });
+
 }
+
+const findAndDelete = function (id) {
+  store.bookmarks = store.bookmarks.filter(bookmark => bookmark.id !== id);
+};
+
+
+const findById = function (id) {
+  return store.bookmarks.find(bookmark => bookmark.id === id);
+};
 
 function render() {
+  const bookmarkItemString = generateBookmarkString();
+  $('.results').html(bookmarkItemString)
+}
+
+function handleDeleteItemClicked() {
+  $('.results').on('click', '#delete-bookmark', function () {
+    const bmId = $(this).closest('.bookmark-item').attr('data-item-id');
+    api.deleteItem(bmId, () => {
+      findAndDelete(bmId);
+      render();
+    });
+  });
 
 }
 
-function handleNewItemSubmit(){
-  $('form').submit(function (event) {
-    event.preventDefault();
-    const title = $('label #title').val();
-    console.log(title);
+function expandedViewToggleClicked() {
+  $('.results').on('click', '#expand-bookmark', function () {
+    const bmId = $(this).closest('.bookmark-item').attr('data-item-id');
+    const getId = findById(bmId);
+    getId.expandedView = !getId.expandedView;
+    render();
+    //  console.log(getId)
+
   });
 }
 
-handleNewItemSubmit();
+function handleNewItemSubmit() {
+  $('form').submit(function (e) {
+    e.preventDefault();
+    const title = $('label #title').val();
+    const url = $('label #link').val();
+    const desc = $('label #description').val();
+    const rating = $('#rating').val();
+
+    api.createItem({ title, url, desc, rating }, (item) => {
+      addBookmarksToStore(item);
+      render();
+
+    });
+
+  });
+}
+
+
+
+function addBookmarksToStore(item) {
+  store.bookmarks.push(item);
+  item.expandedView = false;
+}
+
+$(document).ready(function () {
+  handleNewItemSubmit();
+  api.getItems((items) => {
+    console.log(items);
+    items.forEach((item) => store.bookmarks.push(item));
+    render();
+  });
+  handleDeleteItemClicked()
+  expandedViewToggleClicked()
+  // handleDeleteItemClicked();
+
+});
