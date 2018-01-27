0703b05708c5096f1de884c04b8232616fed4a7a
DhungelS <32339127+DhungelS@users.noreply.github.com>
2018-01-27 10:15:25 -0500

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,6 @@
 /* global Bookmarks, api, Store*/
 'use strict';
-<<<<<<< HEAD
-=======
 
->>>>>>> 7aab5b4df5744301f6fd823f072c4a51ab261e27
 $(document).ready(function () {
   Bookmarks.bindEventListeners();
   Bookmarks.render();
